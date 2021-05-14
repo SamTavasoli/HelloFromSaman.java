d2aa0ecfec655d48143d2199254de11666164b16
Saman Tavasoli <samsamtava@gmail.com>
2021-05-13 19:55:38 -0700

    Initial commit

---
+++ src/prove04Tavasoli/HelloFromSaman.java
@@ -0,0 +1,8 @@
+package prove04Tavasoli;
+
+public class HelloFromSaman {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("Hello, my name is Saman!");
+    }
+}
