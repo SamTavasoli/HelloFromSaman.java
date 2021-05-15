7e80b332106d6d2fc2e16ad4b022adcb258d828e
cchileshe <chi06001@byui.edu>
2021-05-15 12:38:35 +0200

    This is my update

--- src/prove04Tavasoli/HelloFromSaman.java
+++ src/prove04Tavasoli/HelloFromSaman.java
@@ -5,5 +5,6 @@ public class HelloFromSaman {
         System.out.println();
         System.out.println("Hello, my name is Saman!");
         System.out.println("Hello Saman, my name is Lindsey M.!");
+        System.out.println("Hello Lindsey, my name is Claudia");
     }
 }
